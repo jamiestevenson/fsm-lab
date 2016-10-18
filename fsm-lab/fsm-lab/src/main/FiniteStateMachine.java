af970e5aedad5c09819ae215f47bf80171ef212a
Jamie Stevenson <falteringlemniscate@gmail.com>
2016-10-18 23:41:05 +0100

    First test

---
+++ fsm-lab/fsm-lab/src/main/FiniteStateMachine.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * @author Jamie Stevenson
+ *
+ * This class represents a simple finite state machine in the style of a cell from Conways game of
+ * life.
+ *
+ */
+
+public class FiniteStateMachine {
+
+}
---
+++ fsm-lab/fsm-lab/src/test/FiniteStateMachineTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+import main.FiniteStateMachine;
+
+import org.junit.Test;
+
+public class FiniteStateMachineTest {
+
+	@Test
+	public void fsmConstructTest() {
+		FiniteStateMachine fsm = new FiniteStateMachine();
+	}
+
+}
