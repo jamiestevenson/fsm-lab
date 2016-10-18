89bc9c263fed5ccc4b3b21927b21064edcb89875
Jamie Stevenson <falteringlemniscate@gmail.com>
2016-10-18 23:53:31 +0100

    Added documentation

--- fsm-lab/fsm-lab/src/main/FiniteStateMachine.java
+++ fsm-lab/fsm-lab/src/main/FiniteStateMachine.java
@@ -3,11 +3,15 @@ package main;
 /**
  * @author Jamie Stevenson
  *
- * This class represents a simple finite state machine in the style of a cell from Conways game of
+ * This class represents a simple finite state machine in the style of a cell from Conway's game of
  * life.
- *
  */
 
 public class FiniteStateMachine {
 
+	// Default Constructor
+	public FiniteStateMachine () {
+		
+	}
+	
 }
