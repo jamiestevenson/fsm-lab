89da835f71a792f8301d9a80347a77b1bd8ddda5
Jamie Stevenson <falteringlemniscate@gmail.com>
2016-10-20 22:10:46 +0100

    Loom - add sized constructor

--- fsm-lab/fsm-lab/src/main/Loom.java
+++ fsm-lab/fsm-lab/src/main/Loom.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.awt.Dimension;
+
 /**
  * @author Jamie Stevenson
  * 
@@ -9,5 +11,35 @@ package main;
  */
 
 public class Loom {
+	
+	private final static int MIN_DIMENSION = 1;
+	
+	private int width;
+	private int depth;
+	
+	public Loom () {
+		this(MIN_DIMENSION,MIN_DIMENSION);
+	}
 
+	/**
+	 * Constructor for specified-size Loom
+	 * @param width - the number of FSMs accross in the container
+	 * @param depth - the numner of FSMs down in the container
+	 *
+	 * NB - the minimum size for either dimension is one, this value will be set in the absence of
+	 *      legal value.
+	 */
+	public Loom (int width, int depth) {
+		this.width = validDimension(width);
+		this.depth = validDimension(depth);
+	}
+
+	public Dimension dimension () {
+		return new Dimension(width, depth);
+	}
+	
+	private int validDimension(int d) {
+		return d < MIN_DIMENSION ? MIN_DIMENSION : d;
+	}
+	
 }
--- fsm-lab/fsm-lab/src/test/LoomTest.java
+++ fsm-lab/fsm-lab/src/test/LoomTest.java
@@ -1,6 +1,9 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import java.awt.Dimension;
+
 import main.Loom;
 
 import org.junit.Test;
@@ -8,9 +11,42 @@ import org.junit.Test;
 public class LoomTest {
 
 	@Test
-	public void ConstructTest() {
+	public void DefaultConstructTest () {
 		Loom lm = new Loom();
 		assertNotNull(lm);
+	}
+	
+	@Test
+	public void SpecificConstruct1x1Test () {
+		Loom lm = new Loom(1,1);
+		assertNotNull(lm);
+	}
+	
+	@Test 
+	public void DefaultDimensionTest () {
+		Loom lm = new Loom();
+		Dimension d = lm.dimension();
+		assertNotNull(d);
+		assertTrue(1 == d.getHeight());
+		assertTrue(1 == d.getWidth());
+	}
+	
+	@Test 
+	public void SpecifiedDimensionTest () {
+		Loom lm = new Loom(3,3);
+		Dimension d = lm.dimension();
+		assertNotNull(d);
+		assertTrue(3 == d.getHeight());
+		assertTrue(3 == d.getWidth());
+	}
+	
+	@Test 
+	public void InvalidDimensionTest () {
+		Loom lm = new Loom(0,0);
+		Dimension d = lm.dimension();
+		assertNotNull(d);
+		assertTrue(1 == d.getHeight());
+		assertTrue(1 == d.getWidth());
 	}
 
 }
