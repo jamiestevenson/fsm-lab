8d78a3b6f4a14a5933f7e39dd347af7ebd02aa78
Jamie Stevenson <falteringlemniscate@gmail.com>
2017-03-25 10:33:48 +0000

    Updated with  new interfaces

--- fsm-lab/fsm-lab/src/test/FiniteStateMachineTest.java
+++ fsm-lab/fsm-lab/src/test/FiniteStateMachineTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.*;
+import main.FSM;
 import main.FiniteStateMachine;
 
 import org.junit.Test;
@@ -9,25 +10,25 @@ public class FiniteStateMachineTest {
 
 	@Test
 	public void fsmConstructTest () {
-		FiniteStateMachine fsm = new FiniteStateMachine();
+		FSM fsm = new FiniteStateMachine();
 		assertNotNull(fsm);
 	}
 	
 	@Test
 	public void fsmTickTest () {
-		FiniteStateMachine fsm = new FiniteStateMachine();
+		FSM fsm = new FiniteStateMachine();
 		assertTrue(fsm.tick());
 	}
 	
 	@Test
 	public void fsmGetState () {
-		FiniteStateMachine fsm = new FiniteStateMachine();
+		FSM fsm = new FiniteStateMachine();
 		assertEquals(fsm.state(), "OFF");
 	}
 	
 	@Test
 	public void fsmGetNextState () {
-		FiniteStateMachine fsm = new FiniteStateMachine();
+		FSM fsm = new FiniteStateMachine();
 		assertEquals(fsm.state(), "OFF");
 		fsm.tick();
 		assertEquals(fsm.state(), "ON");
